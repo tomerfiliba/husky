a766c37e432d0040df11a3d177180ba87cf15f71
Tomer Filiba <tomerf@il.ibm.com>
2012-01-22 18:17:35 +0200

    changes

--- src/Construct.hs
+++ src/Construct.hs
@@ -7,7 +7,7 @@ import Data.Int (Int8, Int16, Int32, Int64)
 import Data.Word (Word8, Word16, Word32, Word64)
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Binary as BIN 
-import Data.Bits (Bits, bitSize, shiftR)
+import Data.Bits (Bits, bitSize, shiftR, shiftL)
 import qualified Data.Map as Map
 import qualified Data.Char as Char 
 import Data.Map ((!))
@@ -44,8 +44,8 @@ int64  = primitive :: PU Int64
 lift :: a -> PU a
 lift x = PU snd (\s -> (x,s))
 
-pair :: PU a -> PU b -> PU (a,b)
-pair pa pb = PU (\((va,vb),s) -> let s' = appP pb (vb, s)
+tup2 :: PU a -> PU b -> PU (a,b)
+tup2 pa pb = PU (\((va,vb),s) -> let s' = appP pb (vb, s)
                                  in appP pa (va, s'))
                 (\s -> let (va, s') = appU pa s
                        in let (vb, s'') = appU pb s'
@@ -56,9 +56,42 @@ convert f f' pa = PU (\(vb, s) -> appP pa (f' vb, s))
                      (\s -> let (va, s') = appU pa s
                             in (f va, s'))
 
+tup3 :: PU a -> PU b -> PU c -> PU (a,b,c)
+tup3 pa pb pc = convert (\((a,b),c) -> (a,b,c)) (\(a,b,c) -> ((a,b),c)) $ tup2 (tup2 pa pb) pc
+
+tup4 :: PU a -> PU b -> PU c -> PU d -> PU (a,b,c,d)
+tup4 pa pb pc pd = convert (\((a,b,c),d) -> (a,b,c,d)) (\(a,b,c,d) -> ((a,b,c),d)) $ tup2 (tup3 pa pb pc) pd
+
+tup5 :: PU a -> PU b -> PU c -> PU d -> PU e -> PU (a,b,c,d,e)
+tup5 pa pb pc pd pe = convert (\((a,b,c,d),e) -> (a,b,c,d,e)) (\(a,b,c,d,e) -> ((a,b,c,d),e)) $ tup2 (tup4 pa pb pc pd) pe
+
+(@.) :: PU a -> PU b -> PU (a,b)
+(@.) = tup2
+
+(@|) :: PU a -> (a->b, b->a) -> PU b
+(@|) pa (f, f') = convert f f' pa
+
 repeat :: PU a -> Int -> PU [a]
 repeat pa 0 = lift []
-repeat pa n = convert (\(x,xs) -> (x:xs)) (\(x:xs) -> (x,xs)) (pair pa (repeat pa (n-1)))
+repeat pa n = convert (\(x,xs) -> (x:xs)) (\(x:xs) -> (x,xs)) (tup2 pa (repeat pa (n-1)))
+
+{-
+instance Monad PU where
+    pa >>= fpb = convert snd fpb 
+    return = lift
+
+main = do
+        l <- word8
+        repeat word8 l
+
+-}
+
+{-
+pipe :: PU a -> PU b -> PU c
+pipe pa pb = PU (\((va,vb),s) -> 5)
+                (\s -> let (va, s') = appU pa s
+                       in let 
+-}
 
 
 {-
@@ -132,29 +165,38 @@ data BMPVersion = BMPv1 | BMPv2 deriving (Eq, Ord, Show)
 
 main :: IO ()
 main = do
-        p <- return $ pair word32 word16
+        p <- return $ tup2 word32 word16
         c <- return $ convert (Char.chr . fromIntegral) (fromIntegral . Char.ord) word8
         rc <- return $ repeat c
+        
+        p2 <- return $ word8 @. word16 @. (word32 @| ((`shiftL` 1), (`shiftR` 1))) 
+        
         
         print $ LBS.unpack $ pack p (65, 66)
         print $ unpack p $ LBS.pack [0,0,0,1,0,2]
         print $ pack c 'Z'
         print $ unpack c $ LBS.pack [90]
         print $ LBS.unpack $ pack (repeat word16 2) [13,14]
+        print $ unpack p2 $ LBS.pack [0, 1, 1, 0,0,0,7]
+        
+        {- 
+        print $ LBS.unpack $ pack (tup5 word8 word8 word8 word8 word8) (1,2,3,4,5) 
+        print $ unpack (tup5 word8 word8 word8 word8 word8) $ LBS.pack [1,2,3,4,5] 
     
-    --LBS.putStrLn $ pack (fixedList word32 2) [65, 66, 67]
-    --print $ unpack (fixedList word16 2) (LBS.pack [0, 65, 0, 66, 0, 67])
-    --LBS.putStrLn $ pack (list word8) [65,66,67,68,69]
-    --LBS.putStrLn $ pack ipv4 (IPv4 65 66 67 68)
-    --print $ unpack ipv4 (LBS.pack [127, 0, 0, 1])
-    
-    --m <- return $ LBS.pack [65, 66, 67]
-    --LBS.putStrLn $ pack (magic m) ()
-    --print $ unpack (magic m) (LBS.pack [65, 66, 67, 68])  -- ()
-    --print $ unpack (magic m) (LBS.pack [66, 67, 68, 69])  -- error "basa"
-    
-    --bmpver <- return $ word8 `symmMapping` (Map.fromList [(17, BMPv1), (31, BMPv2)])
-    --print $ unpack bmpver (LBS.pack [31])
-    --print $ pack bmpver BMPv1
+        LBS.putStrLn $ pack (fixedList word32 2) [65, 66, 67]
+        print $ unpack (fixedList word16 2) (LBS.pack [0, 65, 0, 66, 0, 67])
+        LBS.putStrLn $ pack (list word8) [65,66,67,68,69]
+        LBS.putStrLn $ pack ipv4 (IPv4 65 66 67 68)
+        print $ unpack ipv4 (LBS.pack [127, 0, 0, 1])
+           
+        m <- return $ LBS.pack [65, 66, 67]
+        LBS.putStrLn $ pack (magic m) ()
+        print $ unpack (magic m) (LBS.pack [65, 66, 67, 68])  -- ()
+        print $ unpack (magic m) (LBS.pack [66, 67, 68, 69])  -- error "basa"
+            
+        bmpver <- return $ word8 `symmMapping` (Map.fromList [(17, BMPv1), (31, BMPv2)])
+        print $ unpack bmpver (LBS.pack [31])
+        print $ pack bmpver BMPv1 
+        -}
 
 
---
+++ src/run.sh
@@ -0,0 +1,1 @@
+ghc Construct.hs && ./Construct
